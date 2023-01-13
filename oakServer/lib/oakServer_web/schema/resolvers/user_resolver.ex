c33ecb4a959559e5e0b04225c5dd77e8579cfea1
Jacob Mishkin <jacobmishkin@Jacobs-MacBook-Pro.local>
2023-01-13 09:48:18 -0600

    📦 NEW: query all users & remove password from query

--- oakServer/lib/oakServer_web/schema/resolvers/user_resolver.ex
+++ oakServer/lib/oakServer_web/schema/resolvers/user_resolver.ex
@@ -3,6 +3,11 @@ defmodule OakServerWeb.Schema.Resolvers.UserResolver do
   alias OakServerWeb.Utils
   alias OakServerWeb.Constants
 
+  def get_all_users(_, _, _) do
+    users = Auth.list_users()
+    {:ok, users}
+  end
+
   def register_user(_, %{input: input}, _) do
     case Auth.create_user(input) do
       {:ok, _} ->
