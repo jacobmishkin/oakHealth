38ef8ea05a1b5faaafc24cc9aad0976d176c84db
Jacob Mishkin <jacobmishkin@Jacobs-MacBook-Pro.local>
2023-02-24 09:51:00 -0600

    update auth_controller

--- oakServer/lib/oakServer_web/controllers/auth_controller.ex
+++ oakServer/lib/oakServer_web/controllers/auth_controller.ex
@@ -7,17 +7,15 @@ defmodule OakServerWeb.AuthController do
   def register(conn, params) do
     case Auth.create_user(params) do
       {:ok, _} ->
-        render(conn, "acknowledge.json", %{success: true, message: "Registered"})
+        render(conn, "acknowledge.json", %{message: "Registered"})
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "errors.json", %{
-          success: false,
           errors: Utils.format_changeset_errors(changeset)
         })
 
       {_, _} ->
         render(conn, "errors.json", %{
-          success: false,
           message: Constants.internal_server_error()
         })
     end
