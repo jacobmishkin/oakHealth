2b58a54d81d01b0ad632ec97488ed2c8f81330e4
Jacob Mishkin <jacobmishkin@Jacobs-MacBook-Pro.local>
2023-02-27 05:21:17 -0600

    📦 NEW: add get current user to schema and resolver

--- oakServer/lib/oakServer_web/schema/resolvers/user_resolver.ex
+++ oakServer/lib/oakServer_web/schema/resolvers/user_resolver.ex
@@ -1,10 +1,13 @@
 defmodule OakServerWeb.Schema.Resolvers.UserResolver do
   alias OakServer.Auth
 
-  def get_all_users(_, _, %{context: context}) do
-    IO.puts("all user Context => ")
-    IO.inspect(context)
+  # Here for learning
+  def get_all_users(_, _, %{context: _context}) do
     users = Auth.list_users()
     {:ok, users}
+  end
+
+  def get_me(_, _, %{context: %{current_user: current_user}}) do
+    {:ok, current_user}
   end
 end
